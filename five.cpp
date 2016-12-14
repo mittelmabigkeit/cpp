baa4f7a4e82dc16dc2a828396f5347319eea76ea
mittelmabigkeit <comp422012016@mail.ru>
2016-12-14 17:39:58 +0300

    asdasdas

---
+++ five.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Write some number: "<<endl;
+    cin>>a;
+    cout<<"u wrote: "<<a<<endl;
+    system ("pause");
+    return 0;
+}
