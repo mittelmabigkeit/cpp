d7849caf8d51b6506b428e622715740d0d223ca6
mittelmabigkeit <comp422012016@mail.ru>
2016-12-14 17:44:05 +0300

    asdasd

---
+++ seven.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a,b,sum;
+    cout<<"Write first number: "<<endl;
+    cin>>a;
+    cout<<"Write second number: "<<endl;
+    cin>>b;
+    sum = a+b;
+    cout<<sum<<endl;    
+    system ("pause");
+    return 0;
+}
