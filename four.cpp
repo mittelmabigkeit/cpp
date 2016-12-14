cd42373b46deca18057dc669978b47cf32833da3
mittelmabigkeit <comp422012016@mail.ru>
2016-12-14 17:38:38 +0300

    adasdsa

---
+++ four.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string a;
+    cout<<"Write some text: "<<endl;
+    cin>>a;
+    cout<<"u wrote: "<<a<<endl;
+    system ("pause");
+    return 0;
+}
